eff59b9fd9f70a874bb3ab4c8dbe828cc3c9c814
Hienphan0111 <hienphan@digitalcad.edu.vn>
2023-12-27 18:50:07 +0700

    Add logo and menu

--- app/(landing-page)/navbar/_components/logo.tsx
+++ app/(landing-page)/navbar/_components/logo.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 const Logo = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Link href='/'>
+      <Image
+        src='../../../assets/images/logo.svg'
+        alt='logo'
+        width={80}
+        height={80}
+        className='mr-6'
+      />
+    </Link>
+  );
+};
 
-export default Logo
+export default Logo;
--- app/(landing-page)/navbar/_components/menu.tsx
+++ app/(landing-page)/navbar/_components/menu.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from '@/components/ui/navigation-menu';
 
 const Menu = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
 
-export default Menu
+export default Menu;
